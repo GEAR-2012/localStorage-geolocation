102703f31f4b02db68b4b050a7ce491e30ea73be
GEAR-2012 <sandor.tudja@gmail.com>
2020-05-18 22:50:31 +0100

    upload new files

---
+++ index.js
@@ -0,0 +1,50 @@
+/*
+
+
+
+*/
+$(document).ready(function () {
+  // LOCALSTORAGE
+
+  if (
+    localStorage.getItem("first-name").length > 0 &&
+    localStorage.getItem("last-name").length > 0
+  ) {
+    alert(
+      "Welcome on this site " +
+        localStorage.getItem("first-name") +
+        " " +
+        localStorage.getItem("last-name") +
+        " again."
+    );
+  } else {
+    alert("Welcome on this site Adventurer.");
+  }
+  // first name
+  $("#set-first-name").click(function () {
+    let firstName = $("#first-name").val();
+    localStorage.setItem("first-name", firstName);
+    alert(
+      "Your first name is stored in local storage, " +
+        localStorage.getItem("first-name") +
+        "."
+    );
+  });
+
+  $("#set-last-name").click(function () {
+    let lastName = $("#last-name").val();
+    localStorage.setItem("last-name", lastName);
+    alert(
+      "Your last name is stored in local storage, " +
+        localStorage.getItem("last-name") +
+        "."
+    );
+  });
+
+  // GEOLOCATION
+  if ("geolocation" in navigator) {
+    // do something
+  } else {
+    alert("Your browser do not support geolocation API.");
+  }
+});
